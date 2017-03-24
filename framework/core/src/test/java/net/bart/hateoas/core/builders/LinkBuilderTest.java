e9de971e94e25cb9f57a056f12369c431fdcdb8a
zibadian <noone@nowhere.universe>
2017-03-24 22:12:15 +0100

    Removed unused classes

--- framework/core/src/test/java/net/bart/hateoas/core/builders/LinkBuilderTest.java
+++ framework/core/src/test/java/net/bart/hateoas/core/builders/LinkBuilderTest.java
@@ -1,15 +1,8 @@
 package net.bart.hateoas.core.builders;
 
-import net.bart.hateoas.core.HateoasException;
-import net.bart.hateoas.core.HateoasLinksTest;
-import net.bart.hateoas.core.builders.urls.FragmentPart;
-import net.bart.hateoas.core.builders.urls.HostPart;
-import net.bart.hateoas.core.builders.urls.QueryParamPart;
 import net.bart.hateoas.core.builders.urls.UrlPathPart;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.net.URI;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -26,28 +19,11 @@ public class LinkBuilderTest {
 
     @Test
     public void build() {
-        final QueryParamPart test = new QueryParamPart();
-        test.add("test", "").setMustInclude(true);
-
         final String result = fixture
-                .addPart(test)
                 .addPart(new UrlPathPart("somewhere", true))
-                .addPart(new FragmentPart("frag"))
-                .addPart(new HostPart("localhost"))
                 .build();
 
-        assertEquals("http://localhost/somewhere?test=#frag", result);
-    }
-
-    @Test
-    public void add_multiple_hosts() {
-
-        final String result = fixture
-                .addPart(new HostPart("localhost"))
-                .addPart(new HostPart("testhost"))
-                .build();
-
-        assertEquals("http://testhost", result);
+        assertEquals("/somewhere", result);
     }
 
     @Test
@@ -61,7 +37,7 @@ public class LinkBuilderTest {
     }
 
     private class TestLinkBuilder extends LinkBuilder {
-        public TestLinkBuilder() {
+        TestLinkBuilder() {
             setErrorsDetected(true);
         }
     }
