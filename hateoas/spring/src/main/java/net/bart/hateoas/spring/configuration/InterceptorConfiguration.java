fb270494c461bf1969da68f3ee87f9a8f8d7d810
bart <fdsfdlkfdskjl@vfkfdlkfdhjkfdh.fdldsjkfldjgklas>
2017-03-24 18:07:55 +0100

    Linking of custom HateoasContext in spring

--- hateoas/spring/src/main/java/net/bart/hateoas/spring/configuration/InterceptorConfiguration.java
+++ hateoas/spring/src/main/java/net/bart/hateoas/spring/configuration/InterceptorConfiguration.java
@@ -1,19 +1,18 @@
 package net.bart.hateoas.spring.configuration;
 
 import net.bart.hateoas.spring.processors.SpringArgumentReturnProcessor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
-class InterceptorConfiguration extends WebMvcConfigurationSupport {
+class InterceptorConfiguration extends WebMvcConfigurerAdapter {
 
-    @Bean
-    @Autowired
-    public SpringArgumentReturnProcessor hateoasArgumentProcessor(RequestMappingHandlerAdapter requestMappingHandlerAdapter) {
-        return new SpringArgumentReturnProcessor(requestMappingHandlerAdapter.getMessageConverters());
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SpringArgumentReturnProcessor());
     }
 
 }
--- hateoas/spring/src/main/java/net/bart/hateoas/spring/processors/SpringArgumentReturnProcessor.java
+++ hateoas/spring/src/main/java/net/bart/hateoas/spring/processors/SpringArgumentReturnProcessor.java
@@ -2,42 +2,25 @@ package net.bart.hateoas.spring.processors;
 
 import net.bart.hateoas.core.HateoasContext;
 import net.bart.hateoas.core.annotations.Hateoas;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-import org.springframework.web.servlet.mvc.method.annotation.AbstractMessageConverterMethodProcessor;
 
-import java.util.List;
-
-public class SpringArgumentReturnProcessor extends AbstractMessageConverterMethodProcessor {
-
-    @Autowired
-    public SpringArgumentReturnProcessor(final List<HttpMessageConverter<?>> converters) {
-        super(converters);
-    }
+public class SpringArgumentReturnProcessor implements HandlerMethodArgumentResolver {
 
     @Override
     public boolean supportsParameter(final MethodParameter parameter) {
-        return parameter.getMethodAnnotation(Hateoas.class) != null &&
-                HateoasContext.class.isAssignableFrom(parameter.getParameterType());
+        return HateoasContext.class.isAssignableFrom(parameter.getParameterType()) &&
+                parameter.getParameterAnnotation(Hateoas.class) != null;
     }
 
     @Override
-    public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer, final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
+    public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
+                                  final NativeWebRequest webRequest,
+                                  final WebDataBinderFactory binderFactory) throws Exception {
         return new HateoasContext();
-    }
-
-    @Override
-    public boolean supportsReturnType(final MethodParameter returnType) {
-        return false;
-    }
-
-    @Override
-    public void handleReturnValue(final Object returnValue, final MethodParameter returnType, final ModelAndViewContainer mavContainer, final NativeWebRequest webRequest) throws Exception {
-
     }
 
 }
